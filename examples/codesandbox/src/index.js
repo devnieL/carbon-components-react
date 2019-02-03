6a99f226eb09d18b6c7d40119ac23f525c3964cf
Daniel Flores <dnielfs@gmail.com>
2019-02-03 15:53:13 -0500

    chore(readme): update readme

--- examples/codesandbox/src/index.js
+++ examples/codesandbox/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Button } from 'carbon-components-react';
+import { Button } from '@devniel/carbon-components-react';
 
 const App = () => (
   <div>
